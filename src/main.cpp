eda4591acb576756b3d943111dbd18b16791423d
NJNischal <njnischal@gmail.com>
2019-12-09 15:22:25 -0500

    [Task 3.17][main.cpp] Updated implementaiton

--- src/main.cpp
+++ src/main.cpp
@@ -46,37 +46,83 @@ int main(int argc, char* argv[]) {
   TurtlebotController controller;
   ObjectManipulation obm;
   WarehouseLocomotion wareLoco;
+  int loop = 1;
+  Location pickUp;
+  Location dropOff;
   // Get the pick up and drop off points
-  auto pickUp = wareLoco.getPickPoint1();
-  auto dropOff = wareLoco.getDropPoint1();
-  // Initialize the flag status
-  bool flag = true;
-  // Declare and populate the struct.
-  std::vector<Location> order;
-  order.push_back(pickUp);
-  order.push_back(dropOff);
-  std::cout<< "Spawning the object "<< std::endl;
-  obm.showObject(pickUp.x, pickUp.y);
-  // Publish and subscribe for the velocity.
-  controller.readVel();
-  planner.subscriberStatus();
-  for (auto iter = order.begin(); iter != order.end(); iter++) {
-    auto goal = *iter;
-    // Set the goal point
-    planner.setGoalPt(goal.x, goal.y);
-    while (flag) {
-      ros::Rate loop_rate(1);
-      planner.sendGoalPt();
-      controller.writeVel();
-      auto status = planner.getStatusPt();
-      if (status == 3) {
-        flag = false;
-        ROS_INFO("EXITTING LOOP");
-      }
-      ros::spinOnce();
-      loop_rate.sleep();
+  while (loop == 1) {
+    int pickPt;
+    int dropPt;
+    ROS_INFO("ENTER THE PICK UP POINT FROM 1,2,3 :");
+    std::cin >> pickPt;
+    ROS_INFO("ENTER THE DROP OFF PT FROM 1,2,3 :");
+    std::cin >> dropPt;
+    switch (pickPt) {
+      case 1:
+        ROS_INFO("The choice is Point 1");
+        pickUp = wareLoco.getPickPoint1();
+        break;
+      case 2:
+        ROS_INFO("The choice is Point 2");
+        pickUp = wareLoco.getPickPoint2();
+        break;
+      case 3:
+        ROS_INFO("The choice is Point 3");
+        pickUp = wareLoco.getPickPoint3();
+        break;
+      default:
+        ROS_INFO("Invalid Argument.Re-enter choice");
+        std::cin >> pickPt;
     }
-    flag = true;
+    switch (dropPt) {
+      case 1:
+        ROS_INFO("The choice is Point 1");
+        dropOff = wareLoco.getDropPoint1();
+        break;
+      case 2:
+        ROS_INFO("The choice is Point 2");
+        dropOff = wareLoco.getDropPoint2();
+        break;
+      case 3:
+        ROS_INFO("The choice is Point 3");
+        dropOff = wareLoco.getDropPoint3();
+        break;
+      default:
+        ROS_INFO("Invalid Argument.Re-enter choice");
+        std::cin >> dropPt;
+        }
+    // Initialize the flag status
+    bool flag = true;
+    // Declare and populate the struct.
+    std::vector<Location> order;
+    order.push_back(pickUp);
+    order.push_back(dropOff);
+    obm.showObject(pickUp.x, pickUp.y);
+    // Publish and subscribe for the velocity.
+    controller.readVel();
+    planner.subscriberStatus();
+    for (auto iter = order.begin(); iter != order.end(); iter++) {
+      auto goal = *iter;
+      // Set the goal point
+      planner.setGoalPt(goal.x, goal.y);
+      obm.disappearObject();
+      obm.showObject(goal.x, goal.y);
+      while (flag) {
+        ros::Rate loop_rate(1);
+        planner.sendGoalPt();
+        controller.writeVel();
+        auto status = planner.getStatusPt();
+        if (status == 3) {
+          flag = false;
+          ROS_INFO("EXITTING LOOP");
+        }
+        ros::spinOnce();
+        loop_rate.sleep();
+      }
+      flag = true;
+    }
+    ROS_INFO("Do you want to continue (yes-1; No-Anyother Number):");
+    std::cin >> loop;
   }
   return 0;
 }
