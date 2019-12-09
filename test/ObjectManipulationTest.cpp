3e823a85c4a4b855f3199f52fb970407d2e8cf3e
Charan-Karthikeyan <pvcharankarthikeyan@gmail.com>
2019-12-09 00:50:44 -0500

    [Task 2.4][test/ObjectManipulationTest.cpp] Added test for ObjectManipulation class

---
+++ test/ObjectManipulationTest.cpp
@@ -0,0 +1,73 @@
+/**
+ *  MIT License
+ *
+ *  Copyright (c) 2019 Charan Karthikeyan P V, Nagireddi Jagadesh Nischal
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without
+ *  limitation the rights to use, copy, modify, merge, publish, distribute,
+ *  sublicense, and/or sell copies of the Software, and to permit persons to
+ *  whom the Software is furnished to do so, subject to the following
+ *  conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ *  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *  DEALINGS IN THE SOFTWARE.
+ */
+
+/**
+ * @file ObjectManipulationTest.cpp
+ * @author Charan Karthikeyan P V (Driver), Nagireddi Jagadesh Nischal (Navigator)
+ * @copyright MIT License.
+ * @date 27/11/2019
+ * @brief Cpp file for test class to manipulate objects when Turtlebot reaches pick up or drop off
+ * locations in the map
+ */
+
+#include "gtest/gtest.h"
+#include "../include/warehouse_material_handling_turtlebot/ObjectManipulation.h"
+/*
+ * @brief Test function to test the string conversion.
+ */
+TEST(ObjectManipulationTest,stringTest) {
+  ObjectManipulation obs;
+  EXPECT_EQ(obs.setTargetPoint(10),"10");
+}
+/*
+ * @brief Test function to test the object spawn function.
+ */
+TEST(ObjectManipulationTest,showObjectTest) {
+  ObjectManipulation obs;
+  std::string command = "rosrun gazebo_ros spawn_model -file src/warehouse_material_handling_turtlebot/gazebo_models/wood_cube_10cm/model.sdf -sdf -x 0 -y 0 -z 0 -model wood";
+  EXPECT_EQ(obs.showObject(0,0),command);
+}
+/*
+ * @brief Test function for removing the object.
+ */
+TEST(ObjectManipulationTest,disappearObjectTest) {
+  ObjectManipulation obs;
+  std::string command = "rosservice call /gazebo/delete_model model_name: 'wood'";
+  EXPECT_EQ(obs.disappearObject(),command);
+}
+/*
+ * @brief Test function to test the object spawn function.
+ */
+TEST(ObjectManipulationTest,showObjectNETest) {
+  ObjectManipulation obs;
+  EXPECT_EQ(obs.showObject(0,0),"10");
+}
+/*
+ * @brief Test function for removing the object.
+ */
+TEST(ObjectManipulationTest,disappearObjectNETest) {
+  ObjectManipulation obs;
+  EXPECT_EQ(obs.disappearObject(),"0");
+}
